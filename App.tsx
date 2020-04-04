5e141dfa7b7dad9735b41cacf36ac7a04b21855b
skantus <drskantus@hotmail.com>
2020-04-04 23:55:57 +0200

    Add icons and splash screen for iOS project

--- App.tsx
+++ App.tsx
@@ -7,78 +7,33 @@
  */
 
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {Colors} from 'react-native/Libraries/NewAppScreen';
-import Home from 'src/components/Hello';
+import {SafeAreaView, StyleSheet, View, Text} from 'react-native';
 
 const App: () => React.ReactNode = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>NEW YORK</Text>
-            </View>
-
-            <Home name="Alejo" />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        <Text style={styles.text}>NEW YORK</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 1,
+    backgroundColor: '#1a1a1a',
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#1a1a1a',
   },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    marginTop: 20,
-    fontFamily: 'klavika-bold',
+  text: {
+    color: 'white',
+    fontFamily: 'klavika-medium',
     fontSize: 24,
-    fontWeight: '400',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    fontFamily: 'klavika-bold',
-    lineHeight: 21,
-    fontSize: 30,
-    textAlign: 'center',
-    margin: 10,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
   },
 });
 
