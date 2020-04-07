6e57b82acb5623b7cd476c6d39b818a54ba5357a
skantus <drskantus@hotmail.com>
2020-04-07 19:08:51 +0200

    Add mock data list

---
+++ src/api/data/data.ts
@@ -0,0 +1,159 @@
+const data = [
+  {
+    id: 1,
+    title: 'Album 1',
+    imageUri:
+      'https://cdn1.benzinga.com/files/imagecache/1024x768xUP/images/story/2012/downtown-690827_1920.jpg',
+    description: 'Some description added',
+    items: [
+      {
+        name: 'Some text added 1',
+        url: 'https://f4.bcbits.com/img/a2939269798_10.jpg',
+      },
+      {
+        name: 'Some text added 2',
+        url:
+          'https://i.pinimg.com/originals/3a/f0/e5/3af0e55ea66ea69e35145fb108b4a636.jpg',
+      },
+      {
+        name: 'Some text added 3',
+        url:
+          'https://filmmusicreporter.com/wp-content/uploads/2018/05/mosaic.jpg',
+      },
+      {
+        name: 'Some text added 4',
+        url: 'https://m.media-amazon.com/images/I/81qxkUUs5dL._SS500_.jpg',
+      },
+      {
+        name: 'Some text added 5',
+        url: 'https://f4.bcbits.com/img/a2939269798_10.jpg',
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: 'Album 2',
+    imageUri:
+      'https://cdn1.benzinga.com/files/imagecache/1024x768xUP/images/story/2012/downtown-690827_1920.jpg',
+    description: 'Some description added',
+    items: [
+      {
+        name: 'Some text added 1',
+        url: 'https://f4.bcbits.com/img/a2939269798_10.jpg',
+      },
+      {
+        name: 'Some text added 2',
+        url:
+          'https://i.pinimg.com/originals/3a/f0/e5/3af0e55ea66ea69e35145fb108b4a636.jpg',
+      },
+      {
+        name: 'Some text added 3',
+        url:
+          'https://filmmusicreporter.com/wp-content/uploads/2018/05/mosaic.jpg',
+      },
+      {
+        name: 'Some text added 4',
+        url: 'https://m.media-amazon.com/images/I/81qxkUUs5dL._SS500_.jpg',
+      },
+      {
+        name: 'Some text added 5',
+        url: 'https://f4.bcbits.com/img/a2939269798_10.jpg',
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: 'Album 3',
+    imageUri:
+      'https://cdn1.benzinga.com/files/imagecache/1024x768xUP/images/story/2012/downtown-690827_1920.jpg',
+    description: 'Some description added',
+    items: [
+      {
+        name: 'Some text added 1',
+        url: 'https://f4.bcbits.com/img/a2939269798_10.jpg',
+      },
+      {
+        name: 'Some text added 2',
+        url:
+          'https://i.pinimg.com/originals/3a/f0/e5/3af0e55ea66ea69e35145fb108b4a636.jpg',
+      },
+      {
+        name: 'Some text added 3',
+        url:
+          'https://filmmusicreporter.com/wp-content/uploads/2018/05/mosaic.jpg',
+      },
+      {
+        name: 'Some text added 4',
+        url: 'https://m.media-amazon.com/images/I/81qxkUUs5dL._SS500_.jpg',
+      },
+      {
+        name: 'Some text added 5',
+        url: 'https://f4.bcbits.com/img/a2939269798_10.jpg',
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: 'Album 4',
+    imageUri:
+      'https://cdn1.benzinga.com/files/imagecache/1024x768xUP/images/story/2012/downtown-690827_1920.jpg',
+    description: 'Some description added',
+    items: [
+      {
+        name: 'Some text added 1',
+        url: 'https://f4.bcbits.com/img/a2939269798_10.jpg',
+      },
+      {
+        name: 'Some text added 2',
+        url:
+          'https://i.pinimg.com/originals/3a/f0/e5/3af0e55ea66ea69e35145fb108b4a636.jpg',
+      },
+      {
+        name: 'Some text added 3',
+        url:
+          'https://filmmusicreporter.com/wp-content/uploads/2018/05/mosaic.jpg',
+      },
+      {
+        name: 'Some text added 4',
+        url: 'https://m.media-amazon.com/images/I/81qxkUUs5dL._SS500_.jpg',
+      },
+      {
+        name: 'Some text added 5',
+        url: 'https://f4.bcbits.com/img/a2939269798_10.jpg',
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: 'Album 5',
+    imageUri:
+      'https://cdn1.benzinga.com/files/imagecache/1024x768xUP/images/story/2012/downtown-690827_1920.jpg',
+    description: 'Some description added',
+    items: [
+      {
+        name: 'Some text added 1',
+        url: 'https://f4.bcbits.com/img/a2939269798_10.jpg',
+      },
+      {
+        name: 'Some text added 2',
+        url:
+          'https://i.pinimg.com/originals/3a/f0/e5/3af0e55ea66ea69e35145fb108b4a636.jpg',
+      },
+      {
+        name: 'Some text added 3',
+        url:
+          'https://filmmusicreporter.com/wp-content/uploads/2018/05/mosaic.jpg',
+      },
+      {
+        name: 'Some text added 4',
+        url: 'https://m.media-amazon.com/images/I/81qxkUUs5dL._SS500_.jpg',
+      },
+      {
+        name: 'Some text added 5',
+        url: 'https://f4.bcbits.com/img/a2939269798_10.jpg',
+      },
+    ],
+  },
+];
+
+export default data;
